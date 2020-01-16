6a68c49999337475a367f3db1391a331e64d4128
GatesMa <gatesma@foxmail.com>
2020-01-16 20:46:19 +0800

    Java并发-增加修改

--- src/com/gatesma/ConcurrentTest/TestVolatile.java
+++ src/com/gatesma/ConcurrentTest/TestVolatile.java
@@ -10,6 +10,40 @@ package com.gatesma.ConcurrentTest;
  */
 public class TestVolatile {
 
-    
+    public static void main(String[] args) {
+        ThreadDemo td = new ThreadDemo();
+        new Thread(td).start();
+        while(true) {
+            if(td.isFlag()) {
+                System.out.println("---------------");
+                break;
+            }
+        }
+    }
 
+
+
+}
+
+class ThreadDemo implements Runnable {
+    private boolean flag = false;
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        flag = true;
+        System.out.println("Flag = " + isFlag());
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
